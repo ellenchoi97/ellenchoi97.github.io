54d67bf4a5be6cb23f68c6a1ddabc018d8ce23c4
Ellen Choi <ellenchoi97@gmail.com>
2019-09-06 15:35:53 -0700

    added images and edited project description (167 hw3+4)

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ export function initIndex() {
     document.getElementById("167_1_icon").addEventListener("click", function () { nextPage("cse167_hw1.html") });
     document.getElementById("167_2_icon").addEventListener("click", function () { nextPage("cse167_hw2.html") });
     document.getElementById("167_3_icon").addEventListener("click", function () { nextPage("cse167_hw3.html") });
+    document.getElementById("167_4_icon").addEventListener("click", function () { nextPage("cse167_hw4.html") });
 }
 
 /* -------------------------------- */
